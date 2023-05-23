725b23f000c2bd5891f25cbb047f07add40c84e2
Vinicius Sabio <vinicios_sabio@hotmail.com>
2023-05-23 20:34:22 -0300

    Atividade Terminada

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -8,11 +8,12 @@ import { HomePageRoutingModule } from './home-routing.module';
 
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
+import { Geolocation, Position } from '@capacitor/geolocation';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map') mapRef!: ElementRef<HTMLElement>;
+  map!: GoogleMap;
 
-  constructor() {}
+  ionViewWillEnter(){
+    this.createMap();
+  }
 
+  async createMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: -22.4962832,
+          lng: -48.560531,
+        },
+        zoom: 11,
+      },
+    });
+    this.buscarPosicao();
+  }
+
+  async buscarPosicao(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.focarMapa(coordinates);
+    console.log('Current position:', coordinates);
+
+  }
+
+  focarMapa(coordinates: Position){
+    this.map.setCamera({
+      coordinate:{
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude,
+      },
+      zoom: 15,
+      animate: true,
+    });
+    this.adicionarMarcador(coordinates);
+  }
+
+  async adicionarMarcador(coordinates: Position){
+    const markerId = await this.map.addMarker({
+      coordinate:{
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude,
+      },
+    });
+  }
 }
